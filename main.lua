cd55cb47b3b98a5952cfde0d450c2f64301f3e43
StormCat <hsmcraig@gmail.com>
2023-01-07 13:06:22 +1100

    worm body done

--- main.lua
+++ main.lua
@@ -1,13 +1,14 @@
 function love.load()
-	
+	player = Worm:new()
+	player:load(100,100)
 end
 
 function love.update(dt)
-	
+	player:update(dt)
 end
 
 function love.draw()
-	
+	player:draw()
 end
 
 --[[
@@ -30,3 +31,166 @@ end
 	perhaps buildings can give you some form of upgrades as well
 
 ]]--
+
+Points = {
+	pos = {
+		x = 0,
+		y = 0,
+		dirx = 0,
+		diry = 0,
+	},
+	posB = {
+		x = 0,
+		y = 0,
+	},
+	locked = false,
+	w = 20,
+}
+
+Sticks = {
+	pA = {},
+	pB = {},
+	h = 4,
+}
+
+function Points:new(x,y,locked,w)
+	local o = {}
+	setmetatable(o, self)
+	self.__index = self
+	
+	o.pos = {}
+	o.posB = {}
+	o.pos.x = x
+	o.posB.x = x
+	o.pos.y = y
+	o.posB.y = y
+	o.locked = locked
+	o.pos.dirx = 0
+	o.pos.diry = 0
+	
+	if w then
+		o.w = w
+	end
+	
+	return o
+end
+
+function Sticks:new(pA,pB,h)
+	local o = {}
+	setmetatable(o, self)
+	self.__index = self
+	
+	o.pA = pA
+	o.pB = pB
+	if h then
+		o.h = h
+	end
+	
+	return o
+end
+
+Worm = {
+	x = 0,
+	y = 0,
+	w = 120,
+	h = 400,
+	
+	segCount = 20,
+	
+	dirx = 0,
+	diry = 0,
+	
+	points = {},
+	sticks = {},
+	
+	damping = 0.2,
+	numIter = 10,
+}
+
+function Worm:new()
+	local o = {}
+	setmetatable(o, self)
+	self.__index = self
+	
+	return o
+end
+
+function Worm:load(x,y)
+	self.points = {}
+	self.sticks = {}
+	table.insert(self.points,Points:new(x,y,true,self.w/2))
+	self.x = x
+	self.y = y
+	for z = 1,self.segCount-1 do
+		table.insert(self.points,Points:new(x,y+z*self.h/self.segCount,false,self.w/2*(self.segCount-z)/(self.segCount)))
+	end
+	for z = 2,#self.points do
+		table.insert(self.sticks,Sticks:new(self.points[z-1],self.points[z],self.h/self.segCount))
+	end
+end
+
+function Worm:update(dt)
+	self.x,self.y=love.mouse.getPosition()
+	self:bodyUpdate(dt)
+end
+
+function Worm:bodyUpdate(dt)
+	self.x = self.x + self.dirx*dt
+	self.y = self.y + self.diry*dt
+	self.points[1].pos.x = self.x
+	self.points[1].pos.y = self.y
+	self:simulate(dt)
+end
+
+function Worm:simulate(dt)
+	for i,o in pairs(self.points) do
+		if not o.locked then
+			local posBefore = {}
+			posBefore.x = o.pos.x
+			posBefore.y = o.pos.y
+			o.pos.x = o.pos.x + (o.pos.x - o.posB.x)*self.damping
+			o.pos.y = o.pos.y + (o.pos.y - o.posB.y)*self.damping
+			o.posB.x = posBefore.x
+			o.posB.y = posBefore.y
+		end
+	end
+	for z = 1,self.numIter do
+		for i,o in pairs(self.sticks) do
+			local centre = {}
+			centre.x = (o.pA.pos.x + o.pB.pos.x)/2
+			centre.y = (o.pA.pos.y + o.pB.pos.y)/2
+			local dir = {}
+			dir.x = (o.pA.pos.x - o.pB.pos.x)/((o.pA.pos.x-o.pB.pos.x)^2+(o.pA.pos.y-o.pB.pos.y)^2)^0.5
+			dir.y = (o.pA.pos.y - o.pB.pos.y)/((o.pA.pos.x-o.pB.pos.x)^2+(o.pA.pos.y-o.pB.pos.y)^2)^0.5
+			if not o.pA.locked then
+				o.pA.pos.x = centre.x + dir.x * o.h /2
+				o.pA.pos.y = centre.y + dir.y * o.h /2
+			end
+			if not o.pB.locked then
+				o.pB.pos.x = centre.x - dir.x * o.h /2
+				o.pB.pos.y = centre.y - dir.y * o.h /2
+			end
+		end
+	end
+end
+
+function Worm:draw()
+	love.graphics.setColor(1,1,1)
+	for z = 1,#self.sticks-2 do
+		local o = self.sticks[z]
+		local p = self.sticks[z+1]
+		local ang = -math.atan2(o.pB.pos.y-o.pA.pos.y,o.pB.pos.x-o.pA.pos.x)
+		local ang2 = -math.atan2(p.pB.pos.y-p.pA.pos.y,p.pB.pos.x-p.pA.pos.x)
+		love.graphics.polygon("fill",o.pA.pos.x-math.sin(ang)*o.pA.w,o.pA.pos.y-math.cos(ang)*o.pA.w,
+									 o.pA.pos.x+math.sin(ang)*o.pA.w,o.pA.pos.y+math.cos(ang)*o.pA.w,
+									 o.pB.pos.x+math.sin(ang2)*o.pB.w,o.pB.pos.y+math.cos(ang2)*o.pB.w,
+									 o.pB.pos.x-math.sin(ang2)*o.pB.w,o.pB.pos.y-math.cos(ang2)*o.pB.w)
+	end
+	local o = self.sticks[#self.sticks-1]
+	local p = self.sticks[#self.sticks]
+	local ang = -math.atan2(o.pB.pos.y-o.pA.pos.y,o.pB.pos.x-o.pA.pos.x)
+	local ang2 = -math.atan2(p.pB.pos.y-p.pA.pos.y,p.pB.pos.x-p.pA.pos.x)
+	love.graphics.polygon("fill",o.pA.pos.x-math.sin(ang)*o.pA.w,o.pA.pos.y-math.cos(ang)*o.pA.w,
+									 o.pA.pos.x+math.sin(ang)*o.pA.w,o.pA.pos.y+math.cos(ang)*o.pA.w,
+									 o.pB.pos.x,o.pB.pos.y)
+end
