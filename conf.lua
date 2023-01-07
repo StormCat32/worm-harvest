d86013f76f0ca202e462fdaa67cc6e1fc3d350d7
StormCat <hsmcraig@gmail.com>
2023-01-07 11:15:39 +1100

    initial commit

---
+++ conf.lua
@@ -0,0 +1,4 @@
+function love.conf(t)
+	t.window.width = 16*64
+	t.window.height = 9*64
+end
---
+++ main.lua
@@ -0,0 +1,32 @@
+function love.load()
+	
+end
+
+function love.update(dt)
+	
+end
+
+function love.draw()
+	
+end
+
+--[[
+	worm game
+	
+	2 scenes
+	-Eating/Attacking Scene
+	-Building Scene
+
+	In attacking, you go around controlling a worm creature (flash game style) attempting to eat buildings and maybe kill some people
+	Small enemies try and shoot you, either walking around on the ground or standing on buildings
+	They should be pretty easy to kill
+	Perhaps have versions that do not attack and instead run away
+	
+	Once you are done (dead? time ran out?) a small cutscene plays where you spit out the buildings you ate and it transitions
+	
+	To building scene
+	Where you have your own little people and you place the buildings you ate for them
+	once your town is complete you finish the game?
+	perhaps buildings can give you some form of upgrades as well
+
+]]--
